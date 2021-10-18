e28e1251dc6179f3b18905ab6535f93b970ee95b
gsunil1996 <sunil_nj2_085@masai.school>
2021-10-18 15:10:10 +0530

    third commit

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import styles from "./Login.module.css";
+import Cart from './Cart';
 
 function Login() {
+    const initState = {
+        username: "",
+        password:""
+     }
+    
+    const [text, setText] = useState(initState);
+    const [isAuth, setIsAuth] = useState(false);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setText({
+            ...text,
+            [name]: value
+        })
+    }
+
+    const handleClick = () => {
+        const { username, password } = text;
+        if (username == "admin" && password == "admin") {
+            setIsAuth(true);
+        } else {
+            alert("incorrect details entered");
+        }
+    }
+
+    if (isAuth) {
+        return <Cart />
+    }
+    
     return (
         <div>
-            
+            <h2>Login Page</h2>
+            <div>
+                <input type="text" placeholder="Enter Username" name="username" onChange={handleChange}/>
+                <br />
+                <input type="password" placeholder="Enter Password" name="password" onChange={handleChange} />
+                <br />
+                <button onClick={handleClick}>Login</button>
+            </div>
         </div>
     )
 }
