3e60bb08bfebb1d8acfd1bf33b6f5bd4bec707fe
gsunil1996 <sunil_nj2_085@masai.school>
2021-10-18 17:26:21 +0530

    commit-3

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,12 +3,12 @@ import data from "./data.json"
 import { useState } from 'react'
 import { v4 as uuid } from "uuid";
 import Card from './card';
-
+import styles from "./card.module.css"
 function Navbar() {
     const [items, setItems] = useState(data);
 
     return (
-        <div>
+        <div className={styles.nav}>
             {items.map((item, id) => {
                 return <Card key={id} data1={item} />
             })}
--- src/Components/card.jsx
+++ src/Components/card.jsx
@@ -5,8 +5,12 @@ function Card({data1}) {
     return (
       <div>
         <div className={styles.box}>
+          <div>
+            <img src={data1.image_url} alt="image" />
+          </div>
           <p>{data1.product_name}</p>
-          <p>{data1.description}</p>
+                <p>{data1.description}</p>
+                <p>{data1.price}</p>
         </div>
       </div>
     );
